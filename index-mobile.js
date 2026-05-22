44b2c0f70f89cb0674952ec6a8d9be0fa4848f1b
RohitS19K <rohitsolanki7982@gmail.com>
2026-05-22 06:10:55 -0400

    mobile-update

---
+++ index-mobile.js
@@ -0,0 +1,20 @@
+const menuBtn =
+document.querySelector(".menu-btn");
+
+const mobileMenu =
+document.querySelector(".mobile-menu");
+
+const closeMenu =
+document.querySelector(".close-menu");
+
+menuBtn.addEventListener("click",()=>{
+
+  mobileMenu.classList.add("active");
+
+});
+
+closeMenu.addEventListener("click",()=>{
+
+  mobileMenu.classList.remove("active");
+
+});
---
+++ mobile.js
@@ -0,0 +1,11 @@
+const creativeNav =
+document.querySelector(".creative-nav");
+
+const navCenter =
+document.querySelector(".nav-center");
+
+navCenter.addEventListener("click",()=>{
+
+  creativeNav.classList.toggle("active");
+
+});
